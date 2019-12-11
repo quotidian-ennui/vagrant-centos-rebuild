e6e78c91fa596dd2af1f69ddd572b95c5e98ca9a
Lewin Chan <lewin.chan@adaptris.com>
2019-12-11 15:56:35 +0000

    Remove IUS; set version to be the same as the base Centos version.

--- provision.sh
+++ provision.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo rpm -Uhv http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo rpm -Uhv https://centos7.iuscommunity.org/ius-release.rpm
-sudo yum -y install unzip rng-tools wget yum-plugin-fastestmirror yum-plugin-remove-with-leaves yum-plugin-protectbase haveged cifs-utils git2u
+sudo yum -y -q update
+sudo yum -y -q install unzip rng-tools wget yum-plugin-fastestmirror yum-plugin-remove-with-leaves yum-plugin-protectbase haveged cifs-utils
 systemctl enable haveged.service
 systemctl restart haveged.service
 
